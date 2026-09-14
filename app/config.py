e8aa00d668a6b70d19c4e94193f4a79b3861c38c
Pankaj Ramdas Karpe <pankajkarpe86@gmail.com>
2026-09-14 13:58:29 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str | None = None
     SUPABASE_DB_URL: str | None = None
+    CELERY_BROKER_URL: str | None = None
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
